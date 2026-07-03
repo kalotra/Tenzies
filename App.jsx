c529630a11b40b58cca9992d17431cf98a02b410
Kalpesh Kalotra <kalprofessional15@gmail.com>
2026-07-03 08:05:11 -0400

    add random numbers for dice

--- App.jsx
+++ App.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+import Die from "./Components/Die";
+
 export default function App() {
-  return <main>Type something here</main>;
+  /**
+   * Challenge: Create a `Roll Dice` button that will re-roll
+   * all 10 dice
+   *
+   * Clicking the button should generate a new array of numbers
+   * and set the `dice` state to that new array (thus re-rendering
+   * the array to the page)
+   */
+
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  function rollDice() {
+    setDice(generateAllNewDice());
+  }
+
+  const diceElements = dice.map((num) => <Die value={num} />);
+
+  return (
+    <main>
+      <div className="dice-container">{diceElements}</div>
+
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
+    </main>
+  );
 }

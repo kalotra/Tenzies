b7b83674904b8c7f08eeb17ab94276b65715b192
Kalpesh Kalotra <kalprofessional15@gmail.com>
2026-07-03 12:43:30 -0400

    final commit

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 
 export default function App() {
-  const [dice, setDice] = useState(generateAllNewDice());
+  const [dice, setDice] = useState(() => generateAllNewDice());
 
   /**
    * Challenge:
@@ -29,9 +29,13 @@ export default function App() {
   }
 
   function rollDice() {
-    setDice((oldDice) =>
-      oldDice.map((die) => (die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }))
-    );
+    if (gameWon) {
+      setDice(generateAllNewDice());
+    } else {
+      setDice((oldDice) =>
+        oldDice.map((die) => (die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }))
+      );
+    }
   }
 
   function hold(id) {
@@ -47,6 +51,9 @@ export default function App() {
   return (
     <main>
       {gameWon && <Confetti />}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+      </div>
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its current value between
--- Components/Die.jsx
+++ Components/Die.jsx
@@ -4,7 +4,12 @@ export default function Die(props) {
   };
 
   return (
-    <button onClick={props.hold} style={styles}>
+    <button
+      style={styles}
+      onClick={props.hold}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}>
       {props.value}
     </button>
   );

f3a475a1622751da7f8bfe4cf8e0117a14e49adf
Kalpesh Kalotra <kalprofessional15@gmail.com>
2026-07-03 10:46:12 -0400

    add gameWon and new game feature

--- App.jsx
+++ App.jsx
@@ -5,6 +5,20 @@ import { nanoid } from "nanoid";
 export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
 
+  /**
+   * Challenge:
+   * Log "Game won!" to the console only if the 2 winning
+   * conditions are met.
+   *
+   * 1. all the dice are being held, and
+   * 2. all the dice have the same value
+   *
+   * For now, no need to even save a variable!
+   */
+
+  const gameWon =
+    dice.every((die) => die.isHeld) && dice.every((die) => (die.value = dice[0].value));
+
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
@@ -38,7 +52,7 @@ export default function App() {
       </p>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
-        Roll
+        {gameWon ? "New Game" : "Roll"}
       </button>
     </main>
   );

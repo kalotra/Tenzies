87a1830c2e8393739ff97648401f947b6325780e
Kalpesh Kalotra <kalprofessional15@gmail.com>
2026-07-03 10:57:10 -0400

    add Confetti after game won

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Die from "./Components/Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
@@ -45,6 +46,7 @@ export default function App() {
 
   return (
     <main>
+      {gameWon && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its current value between

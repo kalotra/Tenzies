3b88a1d5dce9861ac627f3b7b839cc4751c97bcc
kalotra <kalotrak15@gmail.com>
2026-07-26 09:35:36 -0400

    fix new game button issue

--- App.jsx
+++ App.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react";
-import Die from "./Components/Die";
+import Die from "./Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
   const [dice, setDice] = useState(() => generateAllNewDice());
 
   /**
    * Challenge:
-   * Log "Game won!" to the console only if the 2 winning
-   * conditions are met.
+   * Make it so when the game is over, the "New Game" button
+   * automatically receives keyboard focus so keyboard users
+   * can easily trigger that button without having to tab
+   * through all the dice first.
    *
-   * 1. all the dice are being held, and
-   * 2. all the dice have the same value
+   * Hints:
+   * 1. Focusing a DOM element with the DOMNode.focus() method
+   *    requires accessing the native DOM node. What tool have
+   *    we learned about that allows us to do that?
    *
-   * For now, no need to even save a variable!
+   * 2. Automatically calling the .focus() on a DOM element when
+   *    the game is won requires us to synchronize the local
+   *    `gameWon` variable with an external system (the DOM). What
+   *    tool have we learned about that allows us to do that?
    */
 
   const gameWon =
-    dice.every((die) => die.isHeld) && dice.every((die) => (die.value = dice[0].value));
+    dice.every((die) => die.isHeld) && dice.every((die) => die.value === dice[0].value);
 
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
@@ -28,12 +36,12 @@ export default function App() {
   }
 
   function rollDice() {
-    if (gameWon) {
-      setDice(generateAllNewDice());
-    } else {
+    if (!gameWon) {
       setDice((oldDice) =>
         oldDice.map((die) => (die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }))
       );
+    } else {
+      setDice(generateAllNewDice());
     }
   }
 
@@ -49,7 +57,7 @@ export default function App() {
 
   return (
     <main>
-      {gameWon}
+      {gameWon && <Confetti />}
       <div aria-live="polite" className="sr-only">
         {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
       </div>
